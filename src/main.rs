1d7f6d6b52f19544be49a30ee7e6b1a60bac664e
Tim Jentzsch <commits@timjen.net>
2024-01-24 18:33:58 +0100

    Update to Bevy 0.12

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .add_startup_system(setup)
-        .add_system(pan_zoom_camera_system)
-        .add_system(camera_changed_system)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (pan_zoom_camera_system, camera_changed_system))
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
             custom_size: Some(Vec2::new(50.0, 50.0)),
@@ -38,12 +37,12 @@ pub fn pan_zoom_camera_system(
     let mut scroll = 0.0;
 
     if input_mouse.pressed(pan_button) {
-        for ev in ev_motion.iter() {
+        for ev in ev_motion.read() {
             pan += ev.delta;
         }
     }
 
-    for ev in ev_scroll.iter() {
+    for ev in ev_scroll.read() {
         scroll += ev.y;
     }
 
@@ -54,7 +53,7 @@ pub fn pan_zoom_camera_system(
             transform.translation.x -= pan.x * scale / 4.0;
             transform.translation.y += pan.y * scale / 4.0;
         } else if scroll.abs() > 0.0 {
-            let scale = transform.scale.x - scroll;
+            let scale = transform.scale.x - scroll * 0.1;
             transform.scale = Vec3::new(scale, scale, scale);
         }
     }
